768c226a8876ac6f10c3d720a5dc1163ab305da8
Christine William <christinegeorge59@gmail.com>
2021-12-13 21:16:34 +0200

    creation

---
+++ cocoabar/ios/build/Pods.build/Debug-iphoneos/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Volumes/Work/Development/CocoaBar/cocoabar/ios/build/Pods.build/Debug-iphoneos/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- cocoabar/lib/main.dart
+++ cocoabar/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,119 +15,219 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'CocoaBar',
+      theme: ThemeData(primaryColor: Colors.white),
+      home: const MyHomePage(title: 'CocoaBar'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   var whatsappURl_android = "whatsapp://send?phone=01115060678&text=hello";
   var whatappURL_ios = "https://wa.me/01115060678?text=${Uri.parse("hello")}";
 
-  void _contactViaWhatsApp(context) {
-    String whatsAppUrl = "";
-
-    String phoneNumber = 'your-phone-number';
-    String description = "your-custom-message";
-
+  openwhatsapp() async {
+    var whatsapp = "+201212903697";
+    var whatsappURl_android =
+        "whatsapp://send?phone=" + whatsapp + "&text=I want to order";
+    var whatappURL_ios =
+        "https://wa.me/$whatsapp?text=${Uri.parse("I want to order")}";
     if (Platform.isIOS) {
-      whatsAppUrl =
-          'whatsapp://wa.me/$phoneNumber/?text=${Uri.parse(description)}';
+      // for iOS phone only
+      if (await canLaunch(whatappURL_ios)) {
+        await launch(whatappURL_ios, forceSafariVC: false);
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: new Text("whatsapp no installed")));
+      }
     } else {
-      whatsAppUrl =
-          'https://wa.me/+$phoneNumber?text=${Uri.parse(description)}';
+      // android , web
+      if (await canLaunch(whatsappURl_android)) {
+        await launch(whatsappURl_android);
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: new Text("whatsapp no installed")));
+      }
     }
-
-    if (await canLaunch(whatsAppUrl)) {
-      await launch(whatsAppUrl);
-    } else {
-      final snackBar = SnackBar(
-        content: Text("Install WhatsApp First Please"),
-      );
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: GridView.count(
-        // Create a grid with 2 columns. If you change the scrollDirection to
-        // horizontal, this produces 2 rows.
-        crossAxisCount: 2,
-        // Generate 100 widgets that display their index in the List.
+    var grid = Container(
+      child: GridView.count(
+        crossAxisCount: 5,
         children: [
           Center(
             child: InkWell(
-                child: const Text('Whatsapp'),
-                onTap: () => _contactViaWhatsApp(context)),
+                child: Column(
+                  children: [
+                    FittedBox(
+                      child: Image.asset(
+                        'whatsapp.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
+                onTap: openwhatsapp),
           ),
           Center(
             child: InkWell(
-                child: const Text('Instagram'),
+                child: Column(
+                  children: [
+                    FittedBox(
+                      child: Image.asset(
+                        'instagram.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
                 onTap: () => {launch('https://www.instagram.com/cocoabar_1/')}),
-          )
+          ),
+          Center(
+            child: InkWell(
+                child: Column(
+                  children: [
+                    FittedBox(
+                      child: Image.asset(
+                        'menu.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
+                onTap: () => {launch('https://g.co/kgs/o7sKKn')}),
+          ),
+          Center(
+            child: InkWell(
+                child: Column(
+                  children: [
+                    FittedBox(
+                      child: Image.asset(
+                        'facebook.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
+                onTap: () => {launch('https://www.facebook.com/cocoaabar/')}),
+          ),
+          Center(
+            child: InkWell(
+                child: Column(
+                  children: [
+                    FittedBox(
+                      child: Image.asset(
+                        'location.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
+                onTap: () => {launch('https://g.co/kgs/o7sKKn')}),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'logo.png',
+              width: 350,
+              height: 160,
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: new AssetImage("home.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(60.0),
+          child: Container(
+              decoration: BoxDecoration(
+                color: Colors.brown.shade100.withOpacity(0.4),
+              ),
+              child: Column(
+                children: [
+                  carousel(),
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(50.0),
+                    child: grid,
+                  ))
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
+
+class carousel extends StatelessWidget {
+  var arrayOfImages = [
+    'carousel/1.jpg',
+    'carousel/2.jpg',
+    'carousel/3.jpg',
+    'carousel/4.jpg',
+    'carousel/5.jpg',
+    'carousel/6.jpg',
+    'carousel/7.jpg',
+    'carousel/8.jpg',
+    'carousel/9.jpg'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 400,
+        aspectRatio: 16 / 9,
+        viewportFraction: 0.8,
+        initialPage: 0,
+        enableInfiniteScroll: true,
+        reverse: true,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 2),
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enlargeCenterPage: true,
+        scrollDirection: Axis.horizontal,
+      ),
+      items: arrayOfImages.map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                // decoration: BoxDecoration(color: Colors.amber),
+                child: Image.asset(i));
+          },
+        );
+      }).toList(),
     );
   }
 }
